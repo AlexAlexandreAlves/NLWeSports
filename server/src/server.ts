cd1730254ba865a169225378081b88b81671abed
Alex <alexalexandrealves@gmail.com>
2022-09-19 21:10:51 -0300

    api's initial creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,21 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express()
 
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
 app.get('/ads', (request, response) => {
     return response.json([
-        { id: 1, name: 'Anuncio 1'},
-        { id: 2, name: 'Anuncio 2'},
-        { id: 3, name: 'Anuncio 3'},
-        { id: 4 , name: 'Anuncio 4 '}
+        { id: 1, name: 'Anuncio 1' },
+        { id: 2, name: 'Anuncio 2' },
+        { id: 3, name: 'Anuncio 3' },
+        { id: 4, name: 'Anuncio 4 ' }
     ])
 })
 
