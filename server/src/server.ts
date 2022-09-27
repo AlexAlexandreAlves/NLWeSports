3a2337f17f1d6f5ab0d685855ccaac43e2b23c17
Alex <alexalexandrealves@gmail.com>
2022-09-26 22:19:30 -0300

    feat: create table Game

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,13 +10,21 @@ app.post('/ads', (request, response) => {
     return response.status(201).json([]);
 });
 
-app.get('/ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
+    //const gameId = request.params.id;
+
     return response.json([
         { id: 1, name: 'Anuncio 1' },
         { id: 2, name: 'Anuncio 2' },
         { id: 3, name: 'Anuncio 3' },
         { id: 4, name: 'Anuncio 4 ' }
     ])
+})
+
+app.get('/games/:id/discord', (request, response) => {
+    //const adId = request.params.id;
+
+    return response.json([])
 })
 
 app.listen(3333);
