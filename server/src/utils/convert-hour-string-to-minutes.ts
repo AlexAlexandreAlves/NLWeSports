7e210cb61d3f7120f5b5de2496d1f4166435de5b
Alex <alexalexandrealves@gmail.com>
2022-09-27 22:17:25 -0300

    :feat  backend structure

---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,10 @@
+//Função para converter dados de entrada como horas em minutos
+
+export function convertHourStringToMinutes(hourString: string) {
+    const [hours, minutes] = hourString.split(':').map(Number)
+
+    const minutesAmout = (hours * 60) + minutes;
+
+    return minutesAmout;
+
+}
