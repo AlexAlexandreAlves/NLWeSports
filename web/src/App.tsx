05011cb1351347a42d9a65481f034c5984459c15
Alex <alexalexandrealves@gmail.com>
2022-10-04 21:30:55 -0300

    feat: create Modal and update style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
-import './styles/main.css';
+import { useState, useEffect } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
 import logo from './assets/Logo.svg';
-import { useState, useEffect } from 'react';
-import { User } from 'phosphor-react';
+
+import { GameController, User } from 'phosphor-react';
+import { Input } from './components/Form/Input';
 
 
 interface Game {
@@ -53,8 +55,82 @@ function App() {
         })}
 
       </div>
+      <Dialog.Root>
+        <CreateAdBanner />
 
-      <CreateAdBanner />
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+            <Dialog.Title className='text-3xl font-black'> Publique um anúncio </Dialog.Title>
+
+            <form className="mt-8 flex flex-col gap-4-">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="game" className="font-semibold">Qual o Game?</label>
+                <Input id="game" placeholder="Selecione o game que deseja jogar"></Input>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label htmlFor="">Seu nome (ou nickname)</label>
+                <Input id="name" placeholder='Como te chamam dentro do game?' />
+              </div>
+
+              <div className="grid grid-cols-2 gap-6">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                  <Input id="yearsPlaying" type="number" placeholder="Tudo bem ser 0!" />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="discord">Qual seu usuário?</label>
+                  <Input id="discord" type="text" placeholder="Usuario#00000" />
+                </div>
+              </div>
+
+              <div className="flex gap-6">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="weekDays">Quando costuma jogar?</label>
+
+                  <div>
+                    <button title="Domingo">D</button>
+                    <button title="Segunda">S</button>
+                    <button title="Terça">T</button>
+                    <button title="Quarta">Q</button>
+                    <button title="Quinta">Q</button>
+                    <button title="Sexta">S</button>
+                    <button title="Sabado">S</button>
+                  </div>
+
+                </div>
+
+                <div className='flex flex-col gap-2 flex-1'>
+                  <label htmlFor="hourStart">Qual horário do dia?</label>
+                  <div className='grid grid-cols-2 gap-2'>
+                    <Input id="hourStart" type="time" placeholder="De" />
+                    <Input id="hourEnd" type="time" placeholder="Até" />
+                  </div>
+                </div>
+
+              </div>
+
+              <div>
+                <Input type="checkbox" />
+                Costumo me conectar ao chat de voz
+              </div>
+
+              <footer>
+                <button>Cancelar</button>
+                <button type="submit">
+                  <GameController />
+                  Encontrar
+                </button>
+              </footer>
+            </form>
+
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+
     </div>
 
   )
--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -1,7 +1,7 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
-import '../styles/main.css';
+import * as Dialog from '@radix-ui/react-dialog';
 
-export function CreateAdBanner(){
+export function CreateAdBanner() {
     return (
         <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
             <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
@@ -10,10 +10,10 @@ export function CreateAdBanner(){
                     <span className='text-zinc-400'>Publique um anuncio para encontrar novos players!</span>
                 </div>
 
-                <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+                <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
                     <MagnifyingGlassPlus size={24} />
                     Publicar Anúncio
-                </button>
+                </Dialog.Trigger>
 
             </div>
         </div>
