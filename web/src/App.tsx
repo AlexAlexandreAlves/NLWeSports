f039d8692bf2c680e7336219ec748594aacaf9b9
Alex <alexalexandrealves@gmail.com>
2022-10-11 21:19:13 -0300

    feat: styled button and finish the modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,7 @@ import { CreateAdBanner } from './components/CreateAdBanner';
 
 import logo from './assets/Logo.svg';
 
-import { GameController, User } from 'phosphor-react';
+import { GameController } from 'phosphor-react';
 import { Input } from './components/Form/Input';
 
 
@@ -91,14 +91,55 @@ function App() {
                 <div className="flex flex-col gap-2">
                   <label htmlFor="weekDays">Quando costuma jogar?</label>
 
-                  <div>
-                    <button title="Domingo">D</button>
-                    <button title="Segunda">S</button>
-                    <button title="Terça">T</button>
-                    <button title="Quarta">Q</button>
-                    <button title="Quinta">Q</button>
-                    <button title="Sexta">S</button>
-                    <button title="Sabado">S</button>
+                  <div className='grid grid-cols-4 gap-2'>
+                    <button 
+                    title="Domingo"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      D
+                    </button>
+
+                    <button 
+                    title="Segunda"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      S
+                    </button>
+
+                    <button 
+                    title="Terça"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      T
+                    </button>
+
+                    <button 
+                    title="Quarta"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      Q
+                    </button>
+
+                    <button 
+                    title="Quinta"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      Q
+                    </button>
+
+                    <button 
+                    title="Sexta"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      S
+                    </button>
+
+                    <button 
+                    title="Sabado"
+                    className='w-8 h-8 rounded bg-zinc-900'
+                    >
+                      S
+                    </button>
                   </div>
 
                 </div>
@@ -113,15 +154,21 @@ function App() {
 
               </div>
 
-              <div>
+              <div className='mt-2 flex gap-2 text-sm'>
                 <Input type="checkbox" />
                 Costumo me conectar ao chat de voz
               </div>
 
-              <footer>
-                <button>Cancelar</button>
-                <button type="submit">
-                  <GameController />
+              <footer className='mt-4 flex justify-end gap-4'>
+                
+                <Dialog.Close className='bg-zinc-500 px-5  h-12 rounded-md font-semibold hover:bg-zinc-600'>
+                  Cancelar
+                  </Dialog.Close>
+                
+                <button type="submit"
+                className='bg-violet-500 px-5  h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'
+                >
+                  <GameController size={24} />
                   Encontrar
                 </button>
               </footer>
